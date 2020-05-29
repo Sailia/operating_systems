092c2b4476d3aa7c93b6543e65e676da768e4e15
Skylar <syema.ailia@gmail.com>
2020-05-28 21:59:47 -0500

    new computer who dis

--- FinalProject/src/rrScheduler/MainEngine.java
+++ FinalProject/src/rrScheduler/MainEngine.java
@@ -1,5 +1,38 @@
 package rrScheduler;
-
-public class MainEngine {
-
+import java.io.File;
+import java.util.Scanner;
+public class MainEngine extends Process	{
+	
+	
+	public static void Scheduler(Process[] array, int quantum) {
+		for(int i = 0; i < array.length; i++) {
+			for(int j = i + 1; j < array.length; j++) {
+				if(array[i].getArrivalTime() == array[j].getArrivalTime()) {
+					if(array[i].getID() > array[j].getID()) {
+						System.out.println(array[j].toString());
+					}
+				} else if (array[i].getArrivalTime() < array[j].getArrivalTime()) {
+					System.out.println(array[i].toString());
+				} else if (array[i].getID() < array[j].getID()) {
+					System.out.println(array[i].toString());
+				}
+			}
+		}
+	}
+	
+	public static void Initializer() {
+		Scanner processFile;
+		
+		try {
+			processFile = new Scanner(new File("Processes.txt"));
+		} catch(Exception e) {
+			System.out.println("Could not find file");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
--- FinalProject/src/rrScheduler/Process.java
+++ FinalProject/src/rrScheduler/Process.java
@@ -1,5 +1,57 @@
 package rrScheduler;
 
 public class Process {
-
+	public static int id = 0;
+	public int arrivalTime, executionTime;
+	
+	public Process() {
+		id++;
+		arrivalTime = 0;
+		executionTime = 0;
+	}
+	
+	public Process(int iD, int aT, int eT) {
+		id = iD;
+		arrivalTime = aT;
+		executionTime = eT;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	
+	public int getExecutionTime() {
+		return executionTime;
+	}
+	
+	public void setID(int iD) {
+		id = iD;
+	}
+	
+	public void setArrivalTime(int aT) {
+		arrivalTime = aT;
+	}
+	
+	public void setExecutionTime(int eT) {
+		executionTime = eT;
+	}
+	
+	@Override
+	
+	public String toString() {
+		return "id: " + id + " arrival time: " + arrivalTime + " executionTime: " + executionTime;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
